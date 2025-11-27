549d7c26ceb169c18f63b6bcd81360b8022e73b4
Rohith0750 <rohithsd0222@gmail.com>
2025-11-27 16:37:48 +0530

    Initial commit

---
+++ lib/contract.ts
@@ -0,0 +1,18 @@
+export const contractAddress = "0x3CfC7176Ab2d9317c5C69CF26248348AbAe3a345";
+
+// Export only the ABI array expected by viem/wagmi
+export const contractABI = [
+  {
+    "inputs": [],
+    "name": "getRandomEmoji",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+] as const;
